dfd8dd1486088c34d9ffb93db1dff090d4994f98
unknown <AliceLiu1218>
2023-07-16 07:20:38 -0400

    HOMEPAGE is showing but not note page

--- Develop/server.js
+++ Develop/server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use(routes);
-app.use(express.static('public'));
 
 // app.get('/', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/public/index.html'))
